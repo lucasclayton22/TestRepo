06f826506bdf9627c1db0ec314575425b2a1244c
Lucas Clayton <lucasclayton22@augustana.edu>
2023-09-27 21:16:03 -0500

    Now with dancing chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bird world!");
         Chicken foghorn = new Chicken();
-        foghorn.dance();
+
     }
 }
